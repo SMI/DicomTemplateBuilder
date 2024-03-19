9673bac54bb2a60da47ceb00254b2b6a1560afc7
James Friel <james.friel4@gmail.com>
2024-03-19 12:01:53 +0000

    Bump to .net8 and increase version number (#222)
    * bump dependancies
    * Mark Windows-only builds as such (System.Runtime.Versioning.SupportedOSPlatformAttribute in SharedAssemblyInfo.cs).
    
    ---------
    
    Co-authored-by: James A Sutherland <>
    Co-authored-by: James A Sutherland <j.a.sutherland@dundee.ac.uk>

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,9 @@
 using System.Reflection;
 
+#if WINDOWS
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("windows")]
+#endif
+
 [assembly: AssemblyCompany("Health Informatics Centre, University of Dundee")]
 [assembly: AssemblyProduct("Dicom Template Builder")]
 [assembly: AssemblyCopyright("Copyright (c) 2018 - 2019")]
@@ -7,6 +11,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("2.2.2")]
-[assembly: AssemblyFileVersion("2.2.2")]
-[assembly: AssemblyInformationalVersion("2.2.2")]
+[assembly: AssemblyVersion("2.2.3")]
+[assembly: AssemblyFileVersion("2.2.3")]
+[assembly: AssemblyInformationalVersion("2.2.3")]
